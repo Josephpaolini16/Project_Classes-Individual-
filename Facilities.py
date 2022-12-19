b8820191c82486215da024c1f0006a5ea670e835
Joseph Paolini <73087226+Josephpaolini16@users.noreply.github.com>
2022-12-19 15:09:42 -0800

    Add files via upload

---
+++ Facilities.py
@@ -0,0 +1,23 @@
+class facilities:
+    def __init__(self, facilityName):
+        self.facilityName = facilityName
+
+    def addFacility(self):
+        self.facilityName = input("what is the name of this new facility?")
+
+        return self.facilityName
+
+    def writeListOffacilitiesToFile(self):
+        with open("Project_Data/files/facilities.txt", "a") as f:
+            f.write("\n")
+        with open("Project_Data/files/facilities.txt", "a") as f:
+            f.write(self.facilityName)
+            
+
+    def displayFacilities(self):
+        facilitiesList = open("Project_Data/files/facilities.txt").read().splitlines()
+        for x in facilitiesList:
+            print(x)
+
+
+
